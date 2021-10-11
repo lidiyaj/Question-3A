1af109476ab60bc5be1d57994d97994524a7039b
Lidiya Joseph <lidiyajoseph@gmail.com>
2021-10-11 15:27:42 +0800

    Updated Startup file

--- taskconsumer/Startup.cs
+++ taskconsumer/Startup.cs
@@ -24,7 +24,7 @@ namespace taskconsumer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddHostedService<ConsumerQueue>();
             services.AddControllers();
         }
 
